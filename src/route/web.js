fd89575abb0ed75c5a3e7bc2f933d277cc15a45a
vkq2808 <vkq0919309031@gmail.com>
2024-09-11 18:11:57 +0700

    Update package.json and package-lock.json with nodemailer dependency

--- src/route/web.js
+++ src/route/web.js
@@ -1,9 +1,9 @@
-import express from "express"; //gọi Express
+import express from "express";
 import homeController from "../controllers/homeController"
-import loginController from "../controllers/loginController"; //gọi controller
-import authenticateToken from "../middlewares/authenticateToken"; //gọi middleware
+import loginController from "../controllers/loginController";
+import authenticateToken from "../middlewares/authenticateToken";
 
-let router = express.Router();  //khởi tạo Route
+let router = express.Router();
 const TOKEN_SERCET_KEY = process.env.TOKEN_SERCET_KEY || 'b13d7ff0644a3befebdf10bbc22f89d2fec4f802e0e4ace0ee1f06265dbb6d98'
 
 let initWebRoutes = (app) => {
@@ -14,8 +14,10 @@ let initWebRoutes = (app) => {
     router.post("/login", loginController.handleLogin);
     router.get("/register", loginController.getRegisterPage);
     router.post("/register", loginController.handleRegister);
-    router.get("/forgot-password", loginController.getForgotPasswordPage);
-    router.post("/forgot-password", loginController.handleForgotPassword);
+    router.get("/reset-password", loginController.getEnterEmailPage);
+    router.post("/reset-password", loginController.handleEnterEmailForResetingPassword);
+    router.get("/reset-password/:token", loginController.getResetPasswordPage);
+    router.post("/reset-password/:token", loginController.handleResetPassword);
 
     router.get("/signout", (req, res) => {
         res.clearCookie('token');
