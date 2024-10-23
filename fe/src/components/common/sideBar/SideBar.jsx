089d034cfcc9b6b69112c2abb513c37f59d94ace
Vũ Khánh Quốc <vkq0919309031@gmail.com>
2024-10-23 17:34:48 +0700

    hot fix giao diện

--- fe/src/components/common/sideBar/SideBar.jsx
+++ fe/src/components/common/sideBar/SideBar.jsx
@@ -23,7 +23,7 @@ const SideBar = ({ setIsSideBarOpen, user }) => {
         <div className="flex flex-col w-auto h-full overflow-y-auto z-200">
             <div className="search-container flex flex-row w-auto h-auto mx-2 mb-4">
                 <input type="text" className="SearchBar" placeholder="Search..." content="Hello" />
-                <IconButton iconClassName={"fa-search"} onClick={handleSearch} />
+                <IconButton iconClassName={"fas fa-search"} onClick={handleSearch} />
             </div >
             {sideBarItems.map((item, index) => (
                 <SideBarItem key={index} handleClick={() => handleClick(item.link)} text={item.name} iconClassName={item.iconClassName} />
