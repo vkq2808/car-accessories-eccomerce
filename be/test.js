0ee5e148b8adf26231534846910680fe71186f70
vkq265 <107620341+vkq265@users.noreply.github.com>
2024-11-22 02:34:51 +0700

    Update test.js
    delete keys

--- be/test.js
+++ be/test.js
@@ -6,15 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAXBBktxOuhPTCf-v9M3CtzpbmOwJDoo20",
-  authDomain: "haq-gara.firebaseapp.com",
-  projectId: "haq-gara",
-  storageBucket: "haq-gara.firebasestorage.app",
-  messagingSenderId: "849576604123",
-  appId: "1:849576604123:web:18862fdfcd0557859477fe",
-  measurementId: "G-FDR1JTFEGX"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
