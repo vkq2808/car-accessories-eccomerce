cc5d38409692932644b0d4b26a2921927622ab78
Quoc <vkq0919309031@gmail.com>
2025-07-15 10:24:37 +0700

    fix: update base API URL in imgSrc utility to match the correct environment

--- fe/src/utils/imgSrc.js
+++ fe/src/utils/imgSrc.js
@@ -1,6 +1,7 @@
-const baseApiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000/api/v1';
+const baseApiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001/api/v1';
 
 export const imgSrc = (src) => {
-  // replace http://localhost:3000/api/v1 with the actual API URL in env
-  return src.replace('http://localhost:3000/api/v1', baseApiUrl);
+  // replace http://localhost:3001/api/v1 with the actual API URL in env
+  const res = src.replace('http://localhost:3001/api/v1', baseApiUrl);
+  return res;
 };
